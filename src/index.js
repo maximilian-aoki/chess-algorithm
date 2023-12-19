2d0a192607801458b1263d6346c36c0631a7d597
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2023-12-19 12:35:32 -0500

    design and test graph object creation function for chessboard

--- src/index.js
+++ src/index.js
@@ -2,4 +2,50 @@
 import './static/reset.css';
 import './static/style.css';
 
-console.log('yoo whatup world!');
+// all knight moves expressed as [x, y] transformations
+const knightMoves = [
+  [1, 2],
+  [2, 1],
+  [2, -1],
+  [1, -2],
+  [-1, -2],
+  [-2, -1],
+  [-2, 1],
+  [-1, 2],
+];
+
+// return an adjacency graph where each key is a square on the board, and
+// each value is an array of square keys
+function getBoardGraph(size = 8, moves = knightMoves) {
+  const graph = {};
+  for (let i = 0; i < size; i += 1) {
+    for (let j = 0; j < size; j += 1) {
+      graph[`${i}${j}`] = getSquareAdjacents(i, j, size, moves);
+    }
+  }
+
+  return graph;
+}
+
+// fill in adjacency array for each square created by getBoardGraph() by
+// evaluating if each piece move is valid
+function getSquareAdjacents(i, j, size, moves) {
+  const adjacents = [];
+
+  moves.forEach((move) => {
+    const iTransform = i + move[0];
+    const jTransform = j + move[1];
+    if (
+      iTransform >= 0 &&
+      iTransform <= size - 1 &&
+      jTransform >= 0 &&
+      jTransform <= size - 1
+    ) {
+      adjacents.push(`${iTransform}${jTransform}`);
+    }
+  });
+
+  return adjacents;
+}
+
+const boardGraph = getBoardGraph();
